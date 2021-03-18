52b0440ef3b69f39b7485579c1eddddaac7ca24e
Tolfx <matteus12122@gmail.com>
2021-03-18 20:53:54 +0100

    Modified config

--- src/Config.ts
+++ src/Config.ts
@@ -5,3 +5,5 @@ export const Version: string = (JSON.parse(fs.readFileSync("./package.json"))).v
 export const API_Key: string = process.env.KEY_CODE ? process.env.KEY_CODE : "";
 export const Secret_API_Key: string = process.env.API_SECRET_KEY ? process.env.API_SECRET_KEY : "";
 export const API_Domain: string = process.env.API_DOMAIN ? process.env.API_DOMAIN : "";
+export const Locale: string = process.env.LOCALE ? process.env.LOCALE : "";
+export const Organization_ID: string = process.env.ORGANIZATION_ID ? process.env.ORGANIZATION_ID : "";
